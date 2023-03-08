cce71af07e3e1a31e3cc2157f250ea1f1cca36c0
So <wolfiaspikky@gmail.com>
2023-03-08 08:55:46 +0100

    created skill entity

--- src/controller/SkillController.js
+++ src/controller/SkillController.js
@@ -1,49 +1,49 @@
-const Wilder = require("../entity/Wilder");
+const Skill = require("../entity/Skill");
 const dataSource = require("../utils").dataSource;
 
 module.exports = {
     //methods create
     create: (req, res) => {
         dataSource
-        .getRepository(Wilder)
+        .getRepository(Skill)
         .save(req.body)
         .then(() => {
-            res.send("Created Wilder");
+            res.send("Created Skill");
         })
         .catch(() => {
-            res.send("Error while creating the wilder");
+            res.send("Error while creating the skill");
         });
     },
     //methods read
     read: async (req, res) => {
         try {
-            const allWilders = await dataSource.getRepository(Wilder).find();
-            res.send(allWilders);
+            const allSkills = await dataSource.getRepository(Skill).find();
+            res.send(allSkills);
         } catch (err) {
             console.log(err);
-            res.send("Error while read the wilder");
+            res.send("Error while read the skill");
         }
     },
     //methods delete
     delete: async (req, res) => {
         try {
-            const deleteWilder = await dataSource.getRepository(Wilder).delete(req.params.id);
-            res.send(deleteWilder);
+            const deleteSkill = await dataSource.getRepository(Skill).delete(req.params.id);
+            res.send(deleteSkill);
         } catch (err) {
             console.log(err);
-            res.send("Error while deleted the wilder")
+            res.send("Error while deleted the skill")
         }
     },
     //methods update
     update: async (req, res) => {
         try {
-            const upWilder = await dataSource
-            .getRepository(Wilder)
+            const upSkill = await dataSource
+            .getRepository(Skill)
             .update(req.params.id, req.body);
-            res.send(upWilder);
+            res.send(upSkill);
         } catch (err) {
             console.log(err)
-            res.send("Error while update the wilder")
+            res.send("Error while update the skill")
         }
     },
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dataSource = require("./utils").dataSource;
 const wilderController = require("./controller/WilderController")
+const skillController = require("./controller/SkillController")
 
 const app = express();
 
@@ -13,11 +14,17 @@ app.get("/", (req, res) =>{
     res.send("Hello World");
 });
 
+// for api wilder
 app.post("/api/wilder", wilderController.create);
 app.get("/api/wilder", wilderController.read);
-app.delete("/api/wilder", wilderController.delete);
-app.put("/api/wilder", wilderController.update);
+app.delete("/api/wilder/:id", wilderController.delete);
+app.put("/api/wilder/:id", wilderController.update);
 
+//for api skill
+app.post("/api/skill", skillController.create);
+app.get("/api/skill", skillController.read);
+app.delete("/api/skill/:id", skillController.delete);
+app.put("/api/skill/:id", skillController.update);
 
 //start
 const start = async () => {
